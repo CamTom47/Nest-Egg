b98f6e647206686fe5b8ba7922c917efce5c15c7
Cameron Thomas <camthomas47@gmail.com>
2024-10-28 16:59:38 -0400

    backend config refactored to link to server, category routes and testing completed

--- backend/src/routes/categories/categories.ts
+++ backend/src/routes/categories/categories.ts
@@ -1,18 +1,81 @@
-import db from '../../db'
-import bcrypt from'bcrypt'
-import {
-    ExpressError,
-    NotFoundError,
-    BadRequestError,
-    UnauthorizedError
-} from'../../ExpressError'
+import express, { NextFunction } from 'express';
+import Category from '../../models/category/category';
+import newCategorySchema from '../../schema/category/newCategorySchema.json';
+import updateCategorySchema from '../../schema/category/updateCategorySchema.json';
+import jsonschema from 'jsonschema';
+import mapper from '../../helpers/mapper';
+import { BadRequestError } from '../../ExpressError';
 
-const BCRYPT_WORK_FACTOR = require('../../config.js');
-
-import express from 'express'
+import { ensureLoggedIn, ensureCorrectUserOrAdmin } from '../../middleware/auth';
 
 const router = express.Router();
 
-/** Related functions for category */
+/**
+ * GET /categories => {categories}
+ *
+ * categories = { id, name, description }
+ *
+ * Authorization require: logged in
+ */
 
-class Category
+router.get('/', ensureLoggedIn, async function(req: Request, res: Response, next: Function) {
+  try {
+    let categories;
+    if(req.body.user_id) categories = await Category.findAll(req.body.user_id);
+    else categories = await Category.findAll();
+    return res.json({ categories });
+  } catch (err: Error) {
+    return next(err);
+  }
+});
+
+router.get('/:category_id', ensureLoggedIn, async function(req: Request, res: Response, next: Function) {
+    try {
+
+      const category = await Category.findById(req.params.category_id);
+      return res.status(200).json({ category });
+    } catch (err: Error) {
+      return next(err);
+    }
+  });
+
+  router.post('/', ensureLoggedIn, async function(req: Request, res: Response, next: NextFunction){
+    try{
+        const data = mapper(req.body, "category");
+        const validator = jsonschema.validate({...data, system_default: false}, newCategorySchema)
+        if(!validator.valid){
+            const errs = validator.errors.map( err => err.stack);
+                throw new BadRequestError(errs)
+        }
+        const category = await Category.create(data);
+        return res.status(201).json({category});
+    } catch(err){
+        return next(err)
+    }
+  })
+
+  router.patch('/:category_id', ensureCorrectUserOrAdmin, async function(req: Request, res: Response, next: NextFunction){
+    try{
+        const data = mapper(req.body, "category");
+        const validator = jsonschema.validate(data, updateCategorySchema)
+        if(!validator.valid){
+            const errs = validator.errors.map( err => err.stack);
+                throw new BadRequestError(errs)
+        }
+        const category = await Category.update(req.params.category_id, data);
+        return res.status(200).json({category});
+    } catch(err){
+        return next(err)
+    }
+  })
+
+  router.delete('/:category_id', ensureCorrectUserOrAdmin, async function(req: Request, res: Response, next: NextFunction){
+    try{
+        const category = await Category.delete(req.params.category_id);
+        return res.status(200).json({category});
+    } catch(err){
+        return next(err)
+    }
+  })
+
+export default router;
